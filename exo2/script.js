df1d374bf21d5ddf2e4fcf83b65a3b17965bcb91
jack <bonardi14@gmail.com>
2019-09-13 13:08:19 +0200

    make bar moving

--- exo2/script.js
+++ exo2/script.js
@@ -5,17 +5,32 @@ var DrawBar = function DrawBar(sum,nbr){
     this.nbr = nbr
 }
 
-DrawBar.prototype.createBar = function() {
+DrawBar.prototype.progressBar = function() {
     var container = document.createElement('div');
     var bar = document.createElement('div');
+    var width = 0;
+    var id = setInterval(frame, 50);
     container.style.border = '1px solid';
+    container.style.height = '50px';
     bar.style.width = '0%';
+    bar.style.height = '50px';
+    bar.style.background = 'red'
     document.querySelector('body').appendChild(container);
     container.appendChild(bar);
+    function frame() {
+        if (width >= 100) {
+          clearInterval(id);
+        } else {
+          width++; 
+          bar.style.width = width + '%'; 
+          bar.innerHTML = width * 1 + '%';
+        }
+      }
+      frame();
 }
 
 DrawBar.prototype.render = function() {
-    this.createBar();
+    this.progressBar();
   }
 
 var DrawBar = new DrawBar();
