91dda5cd9eaa40b5b4be06d28e321812af14271f
jack <bonardi14@gmail.com>
2019-10-29 11:17:48 +0100

    update exercise 5

--- exo5/script.js
+++ exo5/script.js
@@ -6,26 +6,34 @@ function checkInput() {
     var prenom = document.querySelector('#prenom');
     var email = document.querySelector('#email');
     var mdp = document.querySelector('#mdp');
-    var error = document.querySelector('#error');
     var success = document.querySelector('#success');
-    var nameRegexp = new RegExp(/([.,;:?!§@àçèé&"'()])/);
+    var nameRegexp = new RegExp(/([.,;:?!§@&"'()])/);
     var emailRegexp = new RegExp(/(\w+\@\w+.com|\w+\@\w+.fr)/);
-    var mdpRegexp = new RegExp(/([a-zA-Z0-9])/);
+    var mdpRegexp = new RegExp(/?=.*([a-zA-ZÀ-ÿ0-9@#!*/$\\%?&]){6,}/);
     
-    if (nom.nameRegexp) {
-        error.innerHTML = '<p>Le champ "nom" contient des caractères non autorisés</p>';
+    if (nameRegexp.exec(nom.value)) {
+        document.querySelector('#pName').innerText = 'Le champ "nom" contient des caractères non autorisés';
     } 
-    else if (prenom.nameRegexp) {
-        error.innerHTML = '<p>Le champ "prénom" contient des caractères non autorisés</p>';
+    else{
+        document.querySelector('#pName').innerText = 'Le champ "nom" est valide';
+    }    
+    if (nameRegexp.exec(prenom.value)) {
+        document.querySelector('#pSurname').innerText = 'Le champ "prenom" contient des caractères non autorisés';
     } 
-    else if (!email.emailRegexp) {
-        error.innerHTML = '<p>Le fomat de l\'email est incorrect</p>';
-    } 
-    else if(!mdp.mdpRegexp){
-        error.innerHTML = '<p>Le mot de passe doit comporter au moin 1 lettre Majuscule, 1 minuscule et 1 chiffre</p>';
+    else{
+        document.querySelector('#pSurname').innerText = 'Le champ "prenom" est correct';
+    }    
+    if (!emailRegexp.exec(email.value)) {
+        document.querySelector('#pEmail').innerText = 'Le fomat de l\'email est incorrect';
     }
     else{
-        success.innerHTML = '<p>Tous les champs sont corrects</p>';
+        document.querySelector('#pEmail').innerText = 'Le fomat de l\'email est correct';
+    }    
+    if(!mdpRegexp.exec(mdp.value)){
+        document.querySelector('#pMdp').innerText = 'Le mot de passe doit comporter au moin 6 caractères, 1 lettre Majuscule, 1 minuscule, 1 chiffre et 1 caractère spécial';
+    }
+    else{
+        document.querySelector('#pMdp').innerText = 'Le mot de passe est correct';
     }    
 }
 submit.addEventListener('click', function(event){
